b8199b6c14b7632197b5a7661d30f5a6fac6c29f
kulaizki <kulaizke@gmail.com>
2024-01-04 23:35:29 +0800

    df & list focus

---
+++ Introduction to R/22_df-type-subset.r
@@ -0,0 +1,12 @@
+# The planets_df data frame from the previous exercise is pre-loaded
+
+# Select first 5 values of diameter column
+planets_df[1:5,"diameter"]
+
+# planets_df is pre-loaded in your workspace
+
+# Select the rings variable from planets_df
+rings_vector <- planets_df[,"rings"]
+  
+# Print out rings_vector
+rings_vector
---
+++ Introduction to R/28_components-subset.r
@@ -0,0 +1,7 @@
+# shining_list is already pre-loaded in the workspace
+
+# Print out the vector representing the actors
+shining_list$act
+
+# Print the second element of the vector representing the actors
+shining_list$act[2]
